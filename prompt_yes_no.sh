76eb64a39c9dc8c198fad9e0e4f0d205683778e4
Michal Svorc <dev@michalsvorc.com>
2024-01-27 12:12:58 +0100

    style(prompt_yes_no): formatting

--- prompt_yes_no.sh
+++ prompt_yes_no.sh
@@ -12,19 +12,18 @@
 #   0 if the user answers 'yes'
 #   1 if the user answers 'no'
 prompt_yes_no() {
-    while true; do
-        read -p "$1 (y/n): " response
-        case "$response" in
-            [Yy]* ) return 0;;
-            [Nn]* ) return 1;;
-            * ) echo "Please answer 'yes' or 'no'.";;
-        esac
-    done
+  while true; do
+    read -pr "$1 (y/n): " response
+    case "$response" in
+    [Yy]*) return 0 ;;
+    [Nn]*) return 1 ;;
+    *) echo "Please answer 'yes' or 'no'." ;;
+    esac
+  done
 }
 
 if prompt_yes_no "Do you want to continue?"; then
-    echo "You chose to continue."
+  echo "You chose to continue."
 else
-    echo "You chose to cancel."
+  echo "You chose to cancel."
 fi
-
